a5b02a945c590512c1a2a89563b54a5e236d56f7
chamas <bryto28@gmail.com>
2021-04-06 19:05:15 -0300

    nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=31):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Rodrigo')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Chamas'
+    print(p.nome)
+    print(p.idade)
